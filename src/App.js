24de5df306177dbb7536356edca49d651f61ccdd
Antonio Drusin <antonio.drusin@gmail.com>
2022-12-27 17:27:17 -0500

    Use hash routing so we can work with github pages hosting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import './App.css';
 import {
-    createBrowserRouter,
+    createHashRouter, generatePath,
     RouterProvider,
 } from "react-router-dom";
 import Home, {homeLoader} from "./home/Home"
 import Visualizer, {visualizerLoader} from "./visualizer/Visualizer";
 import Controller, {controllerLoader} from "./controller/Controller";
-
+import { HashRouter } from "react-router-dom";
 export default App;
 
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home/>,
-        loader: homeLoader,
-    },
-    { path: "visualizer", element: <Visualizer/>, loader: visualizerLoader},
-    { path: "controller", element: <Controller/>, loader: controllerLoader}
+const router = createHashRouter([
+    {path: "visualizer", element: <Visualizer/>, loader: visualizerLoader},
+    {path: "controller", element: <Controller/>, loader: controllerLoader},
+    {path: "/", element: <Home/>, loader: homeLoader},
 ]);
+
+console.log(generatePath("visualizer"));
 
 function App() {
     return (
