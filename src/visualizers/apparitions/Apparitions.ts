5777a11d4ee02d7e8fdb586bba9a4becbb5ff09c
Antonio Drusin <antonio.drusin@gmail.com>
2023-03-19 11:04:00 -0400

    Conway game of life

--- src/visualizers/apparitions/Apparitions.ts
+++ src/visualizers/apparitions/Apparitions.ts
@@ -6,36 +6,42 @@ import * as THREE from "three";
 import fragmentShaderFile from './shaders/fragment.frag';
 import vertexShaderFile from './shaders/vertex.vert';
 import panda from './images/panda.jpg';
-import {ShaderMaterial} from "three";
+import {
+    Mesh, MeshBasicMaterial,
+    MeshNormalMaterial,
+    NearestFilter,
+    PlaneGeometry,
+    ShaderMaterial,
+    Vector2,
+    WebGLRenderTarget
+} from "three";
 
 @visualizer("Apparitions", "2d")
 export class Apparitions implements IVisualizer {
-    @numberInput("Blue", 0, 1)
+    @numberInput("Blue", 0, 10)
     blue: number = 1;
 
-    private readonly scene: THREE.Scene;
-    private readonly camera: THREE.PerspectiveCamera;
+    private scene?: THREE.Scene;
+    private camera?: THREE.OrthographicCamera;
     private readonly renderer: THREE.WebGLRenderer;
 
     private frameId: number | null = null;
     private material?: ShaderMaterial;
+    private width: number;
+    private height: number;
+    private mesh?: Mesh<PlaneGeometry, MeshBasicMaterial>;
+    private bufferScene?: THREE.Scene;
+    private bufferTextureA?: WebGLRenderTarget;
+    private bufferTextureB?: WebGLRenderTarget;
 
     constructor(width: number, height: number, element: Element) {
-        console.log(width);
-        console.log(height);
-        this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 10);
-        this.camera.position.set(0,0,4);
+        this.width = width;
+        this.height = height;
 
-        this.renderer = new THREE.WebGLRenderer({antialias: true});
-        this.renderer.setSize(width, height);
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(width, height, false);
 
         element.appendChild(this.renderer.domElement);
-    }
-
-    private render(): void {
-        this.material!.uniforms.opacity.value = this.blue;
-        this.renderer.render(this.scene, this.camera);
     }
 
     pause(): void {
@@ -54,33 +60,70 @@ export class Apparitions implements IVisualizer {
         this.frameId = requestAnimationFrame(renderLoop);
     }
 
+    private render(): void {
+        // renders A->B then swaps
+        this.material!.uniforms.opacity.value = this.blue;
+
+        this.material!.uniforms!.tex = {value: this.bufferTextureA!.texture!};
+        this.renderer.setRenderTarget(this.bufferTextureB!);
+        this.renderer.render(this.bufferScene!, this.camera!);
+
+
+        this.mesh!.material!.map = this.bufferTextureB!.texture;
+        this.renderer.setRenderTarget(null);
+        this.renderer.render(this.scene!, this.camera!);
+
+        const tA = this.bufferTextureA;
+        this.bufferTextureA = this.bufferTextureB;
+        this.bufferTextureB = tA;
+        this.material!.uniforms.iTime.value = 2.0;
+
+    }
+
     async load(): Promise<void> {
-        console.log('loading');
-        const geometry = new THREE.PlaneGeometry();
-        const textureLoader = new THREE.TextureLoader();
-        const texture = textureLoader.load(panda);
+
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.OrthographicCamera(-this.width / 2, this.width / 2, -this.height/2 , this.height/2 , 1, 1000);
+        this.camera.position.set(0, 0, -100);
+        //this.camera.lookAt(0, 0, 0)
+        this.camera.rotateX(Math.PI);
+
+
+        this.bufferScene = new THREE.Scene();
+        this.bufferTextureA = new THREE.WebGLRenderTarget(this.width, this.height, {
+            minFilter: THREE.LinearFilter,
+            magFilter: THREE.NearestFilter
+        });
+        this.bufferTextureB = new THREE.WebGLRenderTarget(this.width, this.height, {
+            minFilter: THREE.LinearFilter,
+            magFilter: THREE.NearestFilter
+        });
+        const plane = new THREE.PlaneGeometry(this.width, this.height, 1, 1);
+
         const fileLoader = new THREE.FileLoader();
 
         const vertexShader = await fileLoader.loadAsync(vertexShaderFile) as string;
         const fragmentShader = await fileLoader.loadAsync(fragmentShaderFile) as string;
 
-        console.log(JSON.stringify(vertexShader));
 
-        // Create a material with the texture and the custom shaders
         this.material = new THREE.ShaderMaterial({
             uniforms: {
-                tex: { value: texture },
-                opacity: { value: 0.5}
+                tex: {value: this.bufferTextureA.texture},
+                opacity: {value: 0.5},
+                iResolution: {value: new Vector2(this.width, this.height)},
+                iTime: {value: 0},
             },
             vertexShader: vertexShader,
             fragmentShader: fragmentShader
         });
 
+        const bufferObject = new THREE.Mesh(plane, this.material);
+        this.bufferScene.add(bufferObject);
+
+
         // Create a mesh with the material and geometry
-        const mesh = new THREE.Mesh(geometry, this.material);
-         mesh.position.set(0, 0, 0);
-        //mesh.rotation.set(0, 0, 0);
-        this.scene.add(mesh);
-        console.log('loaded');
+        const finalMaterial = new THREE.MeshBasicMaterial({map: this.bufferTextureB.texture});
+        this.mesh = new THREE.Mesh(plane, finalMaterial);
+        this.scene.add(this.mesh);
     }
 }
--- src/visualizers/apparitions/shaders/fragment.frag
+++ src/visualizers/apparitions/shaders/fragment.frag
@@ -1,9 +1,62 @@
 uniform sampler2D tex;
 uniform float opacity;
+uniform vec2 iResolution;
+uniform vec2 texelSize;
 varying vec2 vUv;
+uniform float iTime;
+
+float random (vec2 st) {
+    return fract(sin(dot(st.xy,
+    vec2(12.9898,78.233)))*
+    43758.5453123);
+}
 
 void main() {
-    vec4 texel = texture2D(tex, vUv);
-    texel.r = opacity;
-    gl_FragColor = vec4(texel.rgb, texel.a);
+//    vec4 texel1 = texture2D(tex, vUv);
+//    vec4 texel2 = texture2D(tex, vUv + texelSize * opacity);
+//
+//    //texel.r = opacity;
+//
+//    vec4 texel = mix(texel1, texel2, 0.5);
+//    texel.r += 0.01;
+//    if ( texel.r > 1.0 )  texel.r = 0.0;
+//    gl_FragColor = vec4(texel.rgb, texel.a);
+
+    /*
+    Again, we need to access the texture using coordinates in the range [0, 1]
+*/
+    vec2 uv = vUv.xy;// / iResolution.xy;
+    vec3 color = vec3(0.0);
+    float neighbors = 0.0;
+    for(float i = -1.0; i <= 1.0; i += 1.0)
+    {
+        for( float j = -1.0; j <= 1.0; j += 1.0)
+        {
+            vec2 offset = vec2(i, j) / iResolution.xy;		 // Scale the offset down
+            vec4 lookup = texture(tex, uv + offset); // Apply offset and sample
+            neighbors += lookup.x;							 // Accumulate the result
+        }
+    }
+    float cell = texture(tex, uv).x;
+    if(cell > 0.0) {
+        if(neighbors >= 3.0 && neighbors <= 4.0) {
+            color = vec3(1.0);
+        }
+    } else if(neighbors > 2.0 && neighbors < 4.0) {
+        color = vec3(1.0);
+    }
+
+    if(iTime < 1.0) { // can also use iFrame == 0, but seems less reliable.
+        //color = vec3(texture(tex, vUv.xy / iResolution.xx).x);
+        color = vec3(random(vUv),random(vUv),random(vUv));
+    }
+    else {
+//        vec4 pixel = texture(tex,uv);
+//        color = vec3(pixel.r, pixel.g, pixel.b);
+        //color = vec3(random(vUv),random(vUv),random(uv));
+
+    }
+
+
+    gl_FragColor = vec4(color, 1.0);
 }
