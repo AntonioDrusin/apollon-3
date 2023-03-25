d41685e8a5669fc73f26f4c7bf2aac0b9d34c5e1
Antonio Drusin <antonio.drusin@gmail.com>
2023-03-25 09:15:35 -0400

    Cleanup and make paramenters value match original code

--- src/visualizers/apparitions/Apparitions.ts
+++ src/visualizers/apparitions/Apparitions.ts
@@ -4,27 +4,24 @@ import * as THREE from "three";
 import fragmentShaderFile from './shaders/fragment.frag';
 import vertexShaderFile from './shaders/vertex.vert';
 import copyShaderFile from './shaders/copy_shader.frag';
-import lineShaderFile from './shaders/line_shader.frag';
 import {
     Mesh,
     PlaneGeometry,
     ShaderMaterial,
-    Vector2, Vector4,
+    Vector2, Vector3, Vector4,
     WebGLRenderTarget
 } from "three";
 import {noise2D} from "./Noise";
 
-function distance(x0: number, y0: number, x1: number, y1: number): number {
-    return Math.sqrt(((x0 - x1) ** 2) + ((y0 - y1) ** 2));
-}
+// https://editor.p5js.org/StevesMakerspace/sketches/d0lPUJt8T
+//
+// TODO:
+//
+// Match ink values to the p5 version.
+// Add runny colors false
+// Name shaders and textures correctly
+// Shorten functions
 
-function map(n: number, fromLow: number, fromHigh: number, toLow: number, toHigh: number): number {
-    return ((n - fromLow) / (fromHigh - fromLow) * (toHigh - toLow)) + toLow;
-}
-
-function lerp(from: number, to: number, by: number): number {
-    return from * (1 - by) + to * by;
-}
 
 function wrap(value: number, from: number, to: number): number {
     return from + Math.abs((value - from) % (to-from));
@@ -38,23 +35,29 @@ export class Apparitions implements IVisualizer {
     @numberInput("Y coord", 0, 1.0)
     posY: number = 0;
 
+    // Parameters
+    private noiseCoordOffset = 0.08;
+    private noiseColorOffset = 0.5;
+    private pixelSkip = 12;
+    private colorSkip = 7;
+    private dryRate = 0.0012
+    private paintDrop = 40;
+
+
+
     private readonly renderer: THREE.WebGLRenderer;
     private scene?: THREE.Scene;
     private bufferScene?: THREE.Scene;
-    private lineScene?: THREE.Scene;
     private camera?: THREE.OrthographicCamera;
 
     private frameId: number | null = null;
     private material?: ShaderMaterial;
     private copy_material?: ShaderMaterial;
-    private line_material?: ShaderMaterial;
     private readonly width: number;
     private readonly height: number;
     private mesh?: Mesh<PlaneGeometry, ShaderMaterial>;
-    private lineMesh?: Mesh<PlaneGeometry, ShaderMaterial>;
     private bufferTextureA?: WebGLRenderTarget;
     private bufferTextureB?: WebGLRenderTarget;
-    private penSize = 10;
 
     // Simulation parameters
     private noiseOffsets = {
@@ -79,10 +82,6 @@ export class Apparitions implements IVisualizer {
         y: 0,
     }
 
-    private posOffset = 0.08;
-    private colorOffset = 0.5;
-    private pixelSkip = 12;
-    private colorSkip = 7;
 
 
     constructor(width: number, height: number, element: Element) {
@@ -117,22 +116,18 @@ export class Apparitions implements IVisualizer {
         this.frameId = requestAnimationFrame(renderLoop);
     }
 
-
     private addPaint(): void {
         this.previousPos.x = this.pos.x;
         this.previousPos.y = this.pos.y;
 
-        this.noiseOffsets.r += this.colorOffset;
-        this.noiseOffsets.g += this.colorOffset;
-        this.noiseOffsets.b += this.colorOffset;
-        this.noiseOffsets.x += this.posOffset;
-        this.noiseOffsets.y += this.posOffset;
+        this.noiseOffsets.r += this.noiseColorOffset;
+        this.noiseOffsets.g += this.noiseColorOffset;
+        this.noiseOffsets.b += this.noiseColorOffset;
+        this.noiseOffsets.x += this.noiseCoordOffset;
+        this.noiseOffsets.y += this.noiseCoordOffset;
 
         this.pos.x += Math.round((noise2D(this.noiseOffsets.x, 0)) * this.pixelSkip);
         this.pos.y += Math.round((noise2D(this.noiseOffsets.y, 0)) * this.pixelSkip);
-
-        //this.pos.x = this.posX * this.width;
-        //this.pos.y = this.posY * this.height;
 
         this.color.r += Math.round((noise2D(this.noiseOffsets.r, 0)) * this.colorSkip);
         this.color.g += Math.round((noise2D(this.noiseOffsets.g, 0)) * this.colorSkip);
@@ -140,42 +135,19 @@ export class Apparitions implements IVisualizer {
         this.color.r = wrap(this.color.r, 100, 255);
         this.color.g = wrap(this.color.g, 100, 255);
         this.color.b = wrap(this.color.b, 100, 255);
-
-        this.drawLinePoints();
-
     }
-
-    private drawLinePoints() {
-        // render the line scene to texture A to add points
-         const dist = distance(this.pos.x, this.pos.y, this.previousPos.x, this.previousPos.y)/(this.penSize/2);
-
-         for (let n=0; n<1; n+=1/dist) {
-             let x = lerp(this.pos.x, this.previousPos.x, n);
-             let y = lerp(this.pos.y, this.previousPos.y, n);
-             x = wrap(x, -this.width / 2, this.width / 2);
-             y = wrap(y, -this.height / 2, this.height / 2);
-            // this.drawPoint(x,y);
-         }
-    }
-
-    private drawPoint(x: number, y: number) {
-        // render the line scene to texture A to add points
-        this.lineMesh!.position.set(x, y, 0);
-        this.lineMesh!.material.uniforms!.color = {value: new Vector4(this.color.r/255, this.color.g/255, this.color.b/255, 0.2)}; // I guess the ink amount is 1.0
-        this.lineMesh!.material.uniforms!.tex = {value: this.bufferTextureA!.texture!};
-        this.renderer.setRenderTarget(this.bufferTextureA!);
-        this.renderer.render(this.lineScene!, this.camera!);
-    }
-
 
     private render(): void {
         this.addPaint();
 
         // Apply the transformation of the material
         this.material!.uniforms!.tex = {value: this.bufferTextureA!.texture!};
-        this.material!.uniforms!.from = {value: new Vector2( Math.abs(this.pos.x % this.width), Math.abs(this.pos.y % this.height))};
-        this.material!.uniforms!.to = {value: new Vector2( Math.abs(this.previousPos.x % this.width), Math.abs(this.previousPos.y % this.height))};
-        this.material!.uniforms!.color = {value: new Vector4(this.color.r/255, this.color.g/255, this.color.b/255, 0.2)};
+        this.material!.uniforms!.from = {value: new Vector2(Math.abs(this.pos.x % this.width), Math.abs(this.pos.y % this.height))};
+        this.material!.uniforms!.to = {value: new Vector2(Math.abs(this.previousPos.x % this.width), Math.abs(this.previousPos.y % this.height))};
+        this.material!.uniforms!.color = {value: new Vector3(this.color.r / 255, this.color.g / 255, this.color.b / 255)};
+        this.material!.uniforms!.dryRate = {value: this.dryRate};
+        this.material!.uniforms!.paintDrop = {value: this.paintDrop};
+
         this.renderer.setRenderTarget(this.bufferTextureB!);
         this.renderer.render(this.bufferScene!, this.camera!);
 
@@ -187,12 +159,7 @@ export class Apparitions implements IVisualizer {
         const tA = this.bufferTextureA;
         this.bufferTextureA = this.bufferTextureB;
         this.bufferTextureB = tA;
-        this.material!.uniforms.iTime.value = 2.0;
-
     }
-
-// https://editor.p5js.org/StevesMakerspace/sketches/d0lPUJt8T
-// https://editor.p5js.org/StevesMakerspace/sketches/d0lPUJt8T
 
     async load(): Promise<void> {
 
@@ -221,17 +188,10 @@ export class Apparitions implements IVisualizer {
         const vertexShader = await fileLoader.loadAsync(vertexShaderFile) as string;
         const fragmentShader = await fileLoader.loadAsync(fragmentShaderFile) as string;
         const copyShader = await fileLoader.loadAsync(copyShaderFile) as string;
-        const lineShader = await fileLoader.loadAsync(lineShaderFile) as string;
 
         this.material = new THREE.ShaderMaterial({
             uniforms: {
-                tex: {value: this.bufferTextureA.texture},
-                opacity: {value: 0.5},
                 iResolution: {value: new Vector2(this.width, this.height)},
-                from: {value: new Vector2(0,0)},
-                to: {value: new Vector2(0,0)},
-                color: {value: new Vector4(0,0,0,0)},
-                iTime: {value: 0},
             },
             vertexShader: vertexShader,
             fragmentShader: fragmentShader,
@@ -247,26 +207,9 @@ export class Apparitions implements IVisualizer {
             depthTest: false,
         });
 
-        this.line_material = new THREE.ShaderMaterial({
-            uniforms: {
-                color: {value: new Vector4(0.5, 1, 1, 0.5)},
-                tex: {value: this.bufferTextureA.texture}
-            },
-            vertexShader: vertexShader,
-            fragmentShader: lineShader,
-            depthTest: false,
-        })
-
         // buffer scene
         const bufferObject = new THREE.Mesh(plane, this.material);
         this.bufferScene.add(bufferObject);
-
-        // line scene
-        this.lineScene = new THREE.Scene();
-        const lineBox = new THREE.PlaneGeometry(this.penSize, this.penSize, 1, 1); // size of the paintbrush
-        this.lineMesh = new THREE.Mesh(lineBox, this.line_material);
-        this.lineMesh.position.set(100, 100, 100);
-        this.lineScene.add(this.lineMesh);
 
         // screen scene
         this.mesh = new THREE.Mesh(plane, this.copy_material);
--- src/visualizers/apparitions/shaders/fragment.frag
+++ src/visualizers/apparitions/shaders/fragment.frag
@@ -1,15 +1,18 @@
 uniform sampler2D tex;
 uniform vec2 iResolution;
-uniform float iTime;
 varying vec2 vUv;
 
-uniform vec4 color;
+// Algorithm parameters
+uniform float dryRate;
+uniform float paintDrop;
 
+// Drawing the line
+uniform vec3 color;
 uniform vec2 from;
 uniform vec2 to;
 
 float drawLine(vec2 p1, vec2 p2) {
-    float Thickness = 0.0003;
+    float thickness = 1.0/(iResolution.x+iResolution.y);
     vec2 uv = vUv.xy;
 
     float a = abs(distance(p1, uv));
@@ -23,66 +26,52 @@ float drawLine(vec2 p1, vec2 p2) {
     // median to (p1, p2) vector
     float h = 2.0 / c * sqrt( p * ( p - a) * ( p - b) * ( p - c));
 
-    return mix(1.0, 0.0, smoothstep(0.5 * Thickness, 1.5 * Thickness, h));
+    return mix(1.0, 0.0, smoothstep(0.5 * thickness, 1.5 * thickness, h));
 }
 
 void main() {
-
-    // spread color around
-
-    // right = avg ( right and me)
-    // left = avg (left and me)
-    // above = avg (above and me)
-    // below = avg(below and me)
-
-    float colorStep = 1.0/200.0;
-    float colorMin = colorStep * 4.0;
-    float dryRate = 1.0/30000.0;
-
-    // my color is the average of the four cardinal directions
     vec2 step = 1.0/iResolution;
 
     float mixers = 1.0;
 
-
     vec4 finalColor = texture(tex, vUv);
-    if ( distance(from / iResolution, to / iResolution)  < 0.5 ) {
+    if ( distance(from / iResolution, to / iResolution)  < 0.9 ) {
         float lineMultiplier = drawLine(from / iResolution, to / iResolution);
         if ( lineMultiplier > 0.0 ) {
-            finalColor = vec4(color.rgb,  finalColor.a + color.a);
+            finalColor = vec4((color.rgb + finalColor.rgb)/2.0,  finalColor.a + paintDrop);
         }
     }
 
     float totalInk = finalColor.a;
+    float inkDisperse = 4.0;
 
-
-    if ( totalInk > colorMin ) totalInk -= colorStep * 4.0;
+    if ( totalInk > inkDisperse ) totalInk -= inkDisperse;
 
     vec4 colorUp = texture(tex, vec2(vUv.x, vUv.y + step.y));
-    if ( colorUp.a > colorMin ) {
+    if ( colorUp.a > inkDisperse ) {
         finalColor.rgb = (finalColor.rgb + colorUp.rgb);
-        totalInk += colorStep;
+        totalInk += 1.0;
         mixers += 1.0;
     }
 
     vec4 colorDown = texture(tex, vec2(vUv.x, vUv.y - step.y));
-    if ( colorDown.a > colorMin ) {
+    if ( colorDown.a > inkDisperse ) {
         finalColor.rgb = (finalColor.rgb + colorDown.rgb);
-        totalInk += colorStep;
+        totalInk += 1.0;
         mixers += 1.0;
     }
 
     vec4 colorRight = texture(tex, vec2(vUv.x + step.x, vUv.y));
-    if ( colorRight.a > colorMin ) {
+    if ( colorRight.a > inkDisperse ) {
         finalColor.rgb = (finalColor.rgb + colorRight.rgb);
-        totalInk += colorStep;
+        totalInk += 1.0;
         mixers += 1.0;
     }
 
     vec4 colorLeft = texture(tex, vec2(vUv.x - step.x, vUv.y));
-    if ( colorLeft.a > colorMin ) {
+    if ( colorLeft.a > inkDisperse ) {
         finalColor.rgb = (finalColor.rgb + colorLeft.rgb);
-        totalInk += colorStep;
+        totalInk += 1.0;
         mixers += 1.0;
     }
 
@@ -90,8 +79,5 @@ void main() {
     finalColor.rgb = finalColor.rgb / mixers;
     finalColor.a = totalInk;
 
-    if ( iTime < 1.0 ) {
-        finalColor = vec4(0.0,0.0,0.0,0.0);
-    }
     gl_FragColor = finalColor;
 }
