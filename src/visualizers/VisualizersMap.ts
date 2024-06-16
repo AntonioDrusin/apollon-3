a727a2367e8df27d32204630e2f3b2916bdd0ea5
Antonio Drusin <antonio.drusin@gmail.com>
2024-06-16 16:04:06 -0400

    Remove unused visualizers

--- src/visualizers/VisualizersMap.ts
+++ src/visualizers/VisualizersMap.ts
@@ -1,10 +1,8 @@
 import {Apparitions} from "./apparitions/Apparitions";
-import {ApparitionsXY} from "./apparitionsXY/ApparitionsXY";
-import {Apparitions_v2} from "./apparitions_v2/Apparitions_v2";
 import {Plots} from "./plots/Plots";
 import {Plexus} from "./plexus/Plexus";
 
 
 export const VisualizersMap = {
-    Apparitions, Apparitions_v2, ApparitionsXY, Plots, Plexus
+    Apparitions, Plots, Plexus
 };
